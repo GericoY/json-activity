16613c863f337a9621fc4e6cb4d4eb50504167e9
GericoY <gericoyutuc@gmail.com>
2023-04-05 07:48:54 +0800

    done php file

---
+++ marvel-heroes.php
@@ -0,0 +1,51 @@
+<?php 
+    header('Access-Control-Allow-Origin: *');
+
+    $marvel_heroes = [
+        [
+            "img" =>
+            "name" => 
+            "species" =>
+            "alias" =>
+            "equipment" =>
+            "appearances" => []
+        ],
+
+        [
+            "img" =>
+            "name" => 
+            "species" =>
+            "alias" =>
+            "equipment" =>
+            "appearances" => []
+        ],
+
+        [
+            "img" =>
+            "name" => 
+            "species" =>
+            "alias" =>
+            "equipment" =>
+            "appearances" => []
+        ],
+
+        [
+            "img" =>
+            "name" => 
+            "species" =>
+            "alias" =>
+            "equipment" =>
+            "appearances" => []
+        ],
+
+        [
+            "img" =>
+            "name" => 
+            "species" =>
+            "alias" =>
+            "equipment" =>
+            "appearances" => []
+        ],
+        
+    ]
+?>
